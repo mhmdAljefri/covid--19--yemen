676b483209bdac48a111eae754940266bc9bfd04
mhmdAljefri <algfry12@gmail.com>
2020-03-23 17:33:55 +0300

    add intro

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,12 @@ import Layout from "../layouts/default"
 import SEO from "../components/seo"
 import CovidStatus from "../containers/CovidStatus"
 import Sponsers from "../containers/Sponsers"
+import Intro from "../containers/Intro"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Intro />
     <CovidStatus />
     <Sponsers />
   </Layout>
